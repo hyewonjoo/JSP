c7d18f848c873b3666b13ee912fd743d3a1e1b0e
hyewonjoo <run98@naver.com>
2022-07-02 01:13:58 +0900

    board project index

---
+++ toy_project_board/Index.java
@@ -0,0 +1,24 @@
+package com.test.toy;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/index.do")
+public class Index extends HttpServlet {
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+
+		//객체생성
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/index.jsp");
+
+		//데이터 전송 - template.jsp에게 
+		dispatcher.forward(req, resp);
+
+	}
+}
