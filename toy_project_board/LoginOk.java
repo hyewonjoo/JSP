90e6d098c4146a29e462e65989db6d4f2699209c
hyewonjoo <run98@naver.com>
2022-07-03 02:40:14 +0900

    board project loginOk

---
+++ toy_project_board/LoginOk.java
@@ -0,0 +1,81 @@
+package com.test.toy.member;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/member/loginok.do")
+public class LoginOk extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		//LoginOk.java
+		//1. 데이터 가져오기
+		//2. DB 작업 > DAO 위임 > select
+		//3. 결과 > 인증 티켓 발급
+		//4. JSP 호출하기
+		
+		
+		HttpSession session = req.getSession();
+		
+		
+		//1.
+		String id = req.getParameter("id");
+		String pw = req.getParameter("pw");
+		
+		//2.
+		MemberDTO dto = new MemberDTO();
+		
+		dto.setId(id);
+		dto.setPw(pw);
+		
+		MemberDAO dao = new MemberDAO();
+		
+		MemberDTO result = dao.login(dto);
+		
+		
+		//3
+		//로그인에 성공했을경우! (아이디와 비밀번호가 일치하는것이 1개라도 있는경우) 
+		if (result != null) {
+			
+			//인증 티켓 발급 !!!!!!*****************
+			session.setAttribute("auth", id);
+			
+			session.setAttribute("name", result.getName());
+			session.setAttribute("lv", result.getLv());
+			
+		}
+		
+		//4.
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/member/loginok.jsp");
+		dispatcher.forward(req, resp);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
