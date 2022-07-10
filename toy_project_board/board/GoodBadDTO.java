783a88404eee0da4343f28e392eafb5dfc71aabe
hyewonjoo <run98@naver.com>
2022-07-11 01:29:55 +0900

    board project : GoodBadDTO

---
+++ toy_project_board/board/GoodBadDTO.java
@@ -0,0 +1,13 @@
+package com.test.toy.board;
+
+import lombok.Data;
+
+@Data
+public class GoodBadDTO {
+	private String seq;
+	private String id;
+	private String bseq;
+	private String good;
+	private String bad;
+	
+}
